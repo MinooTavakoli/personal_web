beaae6da6af5f5bdd474d5734f1bc951b953055d
mi.tavakoli <mi.tavakoli@asiatech.ir>
2022-09-07 11:31:21 +0430

    add jQuery and pages

---
+++ src/components/carousel/Images.js
@@ -0,0 +1,18 @@
+const images = [
+  {
+    id: 1,
+    src: "https://picsum.photos/id/1018/1000/600/",
+    alt: "Image 1",
+  },
+  {
+    id: 2,
+    src: "https://picsum.photos/id/1015/1000/600/",
+    alt: "Image 2 ",
+  },
+  {
+    id: 3,
+    src: "https://picsum.photos/id/1019/1000/600/",
+    alt: "Image 3",
+  },
+];
+export default images;
